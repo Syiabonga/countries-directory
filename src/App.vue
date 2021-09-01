a6cb3e04231e98f039e9961358546e8298df5848
Leandro <syiabonga111@gmail.com>
2021-09-01 11:31:24 -0500

    vuex and axios added to the project

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <Countries/>
+  </div>
+</template>
+
+<script>
+import Countries from './components/Countries.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Countries
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ src/store/modules/countries.js
@@ -0,0 +1,22 @@
+import axios from 'axios'
+
+const state = {
+    countries: []
+};
+
+const getters = {
+    //fuction that gets all the countries
+    //function to delete a country
+    //function to filter countries
+};
+
+const actions = {};
+
+const mutations = {};
+
+export default {
+    state,
+    getters,
+    actions,
+    mutations,
+};
